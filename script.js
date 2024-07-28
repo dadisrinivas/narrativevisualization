1fe58c3a6e6b6242cf0315dfc9d2dd8c27fdc02a
dadisrinivas <srinivasdadi@outlook.com>
2024-07-28 13:11:02 -0500

    Dummy code to check visualizations

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Basic D3 setup to create an SVG and add a text element
+    const data = [
+        { neighbourhood: "Manhattan", price: 200 },
+        { neighbourhood: "Brooklyn", price: 150 },
+        { neighbourhood: "Queens", price: 100 },
+        { neighbourhood: "Bronx", price: 80 },
+        { neighbourhood: "Staten Island", price: 120 }
+    ];
+
     const svg = d3.select("#visualization").append("svg")
         .attr("width", "100%")
         .attr("height", "600px")
         .style("border", "1px solid black");
 
-    d3.csv("data/listings.csv").then(function(data) {
-        console.log(data); // Check the structure of the data
+    // Simple bar chart example
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = 800 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
 
-        // Convert price to number
-        data.forEach(d => {
-            d.price = +d.price; // Ensure price is a number
-        });
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-        // Simple bar chart example
-        const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-        const width = 800 - margin.left - margin.right;
-        const height = 600 - margin.top - margin.bottom;
+    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+    const y = d3.scaleLinear().rangeRound([height, 0]);
 
-        const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+    x.domain(data.map(d => d.neighbourhood));
+    y.domain([0, d3.max(data, d => d.price)]);
 
-        const x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-        const y = d3.scaleLinear().rangeRound([height, 0]);
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
 
-        x.domain(data.map(d => d.neighbourhood));
-        y.domain([0, d3.max(data, d => d.price)]);
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10));
 
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x));
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10));
-
-        g.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", d => x(d.neighbourhood))
-            .attr("y", d => y(d.price))
-            .attr("width", x.bandwidth())
-            .attr("height", d => height - y(d.price))
-            .attr("fill", "steelblue");
-    }).catch(function(error) {
-        console.error('Error loading or parsing data:', error);
-    });
+    g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.neighbourhood))
+        .attr("y", d => y(d.price))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.price))
+        .attr("fill", "steelblue");
 });
